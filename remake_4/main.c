80e50cd0606e2b3dad418658e66851ad6de771f4
Hoyoung Song <hoysong@c3r15s2.42gyeongsan.kr>
2024-06-10 19:25:33 +0900

    remake_4: vld_chk_leak fix

--- remake_4/main.c
+++ remake_4/main.c
@@ -6,10 +6,9 @@
 /*   By: hoysong <hoysong@42gyeongsan.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 14:34:41 by hoysong           #+#    #+#             */
-/*   Updated: 2024/06/10 18:08:17 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/06/10 19:22:56 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "my_fdf.h"
 #define OPEN_ERR		1
 #define FILE_NAME_ERR	2
@@ -53,6 +52,23 @@ int	inpt_hdler(int input, t_mlx_ptrs *mlx_ptrs)
 	return (0);
 }
 
+static void	setup_main(t_mlx_ptrs *mlx_ptrs)
+{
+	mlx_ptrs->init_ptr = mlx_init();
+	if (mlx_ptrs->init_ptr == 0)
+	{
+		mlx_destroy_display(mlx_ptrs->init_ptr);
+		free(mlx_ptrs->init_ptr);
+	}
+	mlx_ptrs->win_ptr = mlx_new_window(mlx_ptrs->init_ptr, 500, 500, "window_1");
+	if (mlx_ptrs->win_ptr == 0)
+	{
+		mlx_destroy_window(mlx_ptrs->init_ptr, mlx_ptrs->win_ptr);
+		mlx_destroy_display(mlx_ptrs->init_ptr);
+		free(mlx_ptrs->init_ptr);
+	}
+}
+
 // ===================================================================================
 
 void	err_hdler(int err_num)
@@ -74,29 +90,7 @@ t_mlx_ptrs	*get_parsed_data(int fd, t_mlx_ptrs *mlx_ptrs)
 	splits = read_file(fd, &file_line_count); // split with ft_split
 	// splits_to_int(splits); // make int arr from splits
 	map_chk_flg = map_vld_chk(splits); // free splits and map vld chk
-	if (map_chk_flg == 0)
-	{
-	}
-	//if (map_vld_chk == 0)
-	//	return (0);
 	return (0);
-}
-
-static void	setup_main(t_mlx_ptrs *mlx_ptrs)
-{
-	mlx_ptrs->init_ptr = mlx_init();
-	if (mlx_ptrs->init_ptr == 0)
-	{
-		mlx_destroy_display(mlx_ptrs->init_ptr);
-		free(mlx_ptrs->init_ptr);
-	}
-	mlx_ptrs->win_ptr = mlx_new_window(mlx_ptrs->init_ptr, 500, 500, "window_1");
-	if (mlx_ptrs->win_ptr == 0)
-	{
-		mlx_destroy_window(mlx_ptrs->init_ptr, mlx_ptrs->win_ptr);
-		mlx_destroy_display(mlx_ptrs->init_ptr);
-		free(mlx_ptrs->init_ptr);
-	}
 }
 
 int	main(int argc, char *argv[])
@@ -111,6 +105,7 @@ int	main(int argc, char *argv[])
 	if (fd < 0)
 		err_hdler(OPEN_ERR);
 	setup_main(&mlx_ptrs);
+	mlx_ptrs.data = 0;
 	mlx_ptrs.data = get_parsed_data(fd, &mlx_ptrs);
 	mlx_hook(mlx_ptrs.win_ptr, KeyPress, KeyPressMask, inpt_hdler, &mlx_ptrs);
 	mlx_loop(mlx_ptrs.init_ptr);
