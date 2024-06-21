10277c3f4dc66254d23cd0402f09d1a8ce4c5f01
hoysong <eagleteo@naver.com>
2024-06-21 20:27:13 +0900

    vlag err fix: when open map

--- remake_11/main.c
+++ remake_11/main.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <hoysong@42gyeongsan.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 14:34:41 by hoysong           #+#    #+#             */
-/*   Updated: 2024/06/20 20:45:58 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/06/21 19:58:56 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "my_fdf.h"
@@ -14,11 +14,20 @@
 #define FILE_NAME_ERR	2
 #define IVLD_MAP		3
 
-typedef struct s_list_1
+typedef struct s_img_struct
+{
+	void	*img_ptr;
+	int		bits_ptr_pixel;
+	int		size_line;
+	int		endian;
+}	t_img_strc;
+
+typedef struct s_mlx_struct
 {
 	void	*init_ptr;
 	void	*win_ptr;
 	t_prs_info	*data;
+	t_img_strc	*img_data;
 }			t_mlx_ptrs;
 
 int	inpt_hdler(int input, t_mlx_ptrs *mlx_ptrs)
@@ -40,12 +49,14 @@ int	inpt_hdler(int input, t_mlx_ptrs *mlx_ptrs)
 
 static void	setup_mlx(t_mlx_ptrs *mlx_ptrs)
 {
+	/* === init_mlx === */
 	mlx_ptrs->init_ptr = mlx_init();
 	if (mlx_ptrs->init_ptr == 0)
 	{
 		mlx_destroy_display(mlx_ptrs->init_ptr);
 		free(mlx_ptrs->init_ptr);
 	}
+	/* === gen win ptr === */
 	mlx_ptrs->win_ptr = mlx_new_window(mlx_ptrs->init_ptr, 500, 500, "window_1");
 	if (mlx_ptrs->win_ptr == 0)
 	{
@@ -67,10 +78,11 @@ void	err_hdler(int err_num, t_mlx_ptrs *mlx_ptrs)
 	else if (err_num == IVLD_MAP)
 	{
 		free_parse_data(mlx_ptrs->data->int_arr, mlx_ptrs->data->horiz, mlx_ptrs->data->vert);
+		free(mlx_ptrs->data);
 		write(1, "[err code: 3] ivld_map\n", 23);
-		mlx_destroy_window(mlx_ptrs->init_ptr, mlx_ptrs->win_ptr);
-		mlx_destroy_display(mlx_ptrs->init_ptr);
-		free(mlx_ptrs->init_ptr);
+//		mlx_destroy_window(mlx_ptrs->init_ptr, mlx_ptrs->win_ptr);
+//		mlx_destroy_display(mlx_ptrs->init_ptr);
+//		free(mlx_ptrs->init_ptr);
 		exit(1);
 	}
 }
@@ -82,41 +94,35 @@ t_prs_info	*get_parsed_data(int fd, t_mlx_ptrs *mlx_ptrs)
 	prs_data = malloc(sizeof(t_prs_info));
 	if (prs_data == 0)
 		return (0);
-
 	prs_data->vert = 0;
 	prs_data->splits = get_splits(fd, &prs_data->vert); // split with ft_split
 	//debug_splits(prs_data->splits);
-
 	prs_data->horiz = count_x_elements((*prs_data->splits));
 	/*=== debug ===*/
 	printf("vert elements: %d\n", prs_data->horiz);
 	printf("hriz elements: %d\n", prs_data->vert);
 	prs_data->int_arr = splits_to_int(prs_data->splits, prs_data->horiz, prs_data->vert);
 	//debug_parsed_data(prs_data->int_arr, prs_data->horiz, prs_data->vert);
-
 	return (prs_data);
 }
 
 int	main(int argc, char *argv[])
 {
-	int		fd;
-	int		i;
-	char	**str;
+	int			fd;
+	int			i;
+	char		**str;
 	t_mlx_ptrs	mlx_ptrs;
 
 	i = 0;
 	if (argc <= 1)
 		err_hdler(OPEN_ERR, 0);
 	fd = open(argv[1], O_RDONLY);
-
 	mlx_ptrs.data = get_parsed_data(fd, &mlx_ptrs);
-
-	setup_mlx(&mlx_ptrs);
 	if (map_vld_chk((mlx_ptrs.data)->splits) == 0)
 		err_hdler(IVLD_MAP, &mlx_ptrs);
-		free_parse_data(mlx_ptrs.data->int_arr, mlx_ptrs.data->horiz, mlx_ptrs.data->vert);
-		free(mlx_ptrs.data);
-
+//	free_parse_data(mlx_ptrs.data->int_arr, mlx_ptrs.data->horiz, mlx_ptrs.data->vert);
+//	free(mlx_ptrs.data);
+	setup_mlx(&mlx_ptrs);
 	mlx_hook(mlx_ptrs.win_ptr, KeyPress, KeyPressMask, inpt_hdler, &mlx_ptrs);
 	mlx_loop(mlx_ptrs.init_ptr);
 }
