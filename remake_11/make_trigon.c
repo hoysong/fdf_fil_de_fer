85cbf8f473f711298b23d1d3d5a562ff721693fc
hoysong_firebat <hoysong@student.42gyeongsan.kr>
2024-07-04 04:09:25 +0900

    make gap 의 문법오류를 다 고친상태입니다. 이제 점들이 중간 갭에 맞게 잘 찍히는지 확인해야 합니다.

--- remake_11/make_trigon.c
+++ remake_11/make_trigon.c
@@ -6,13 +6,13 @@
 /*   By: hoysong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 11:58:31 by hoysong           #+#    #+#             */
-/*   Updated: 2024/07/03 07:37:20 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/07/04 03:27:28 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_fdf.h"
 //#include <math.h>
-void  my_mlx_pixel_put(t_mlx_ptrs *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_mlx_ptrs *data, int x, int y, int color)
 {
 	char *dst;
 
@@ -20,10 +20,30 @@ void  my_mlx_pixel_put(t_mlx_ptrs *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void make_gap(t_mlx_ptrs *mlx_ptrs)
+void	make_gap(t_prs_info *prs_info)
 {
+	int	x;
+	int	y;
+	int gap;
+
+	gap = 10;
+	x = 0;
+	y = 0;
+	while (y < prs_info->vert)
+	{
+		while (x < prs_info->horiz)
+		{
+			prs_info->point[y][x].x = x * gap;
+			prs_info->point[y][x].y = y * gap;
+			prs_info->point[y][x].z = prs_info->int_arr[y][x];
+			x++;
+		}
+		x = 0;
+		y++;
+	}
 	return ;
 }
+
 
 void	make_trigon(t_mlx_ptrs *mlx_ptrs)
 {
@@ -34,7 +54,7 @@ void	make_trigon(t_mlx_ptrs *mlx_ptrs)
 	x = 0;
 	y = 0;
 	gap = 10;
-	make_gap(mlx_ptrs);
+	make_gap(mlx_ptrs->data);
 
 	while (x < mlx_ptrs->data->vert)
 	{
