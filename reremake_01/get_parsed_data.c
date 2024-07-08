3cb2f84a121e5a89fc20e7898b5030645f69d476
hoysong_firebat <hoysong@student.42gyeongsan.kr>
2024-07-09 04:32:25 +0900

    z_atoi와 color_atoi의 코드를 remake_11에서 가져왔다. 잘 작동하는지는 아직 호출을 하지는 않았다. 중간 커밋이다. stable commit.

--- reremake_01/get_parsed_data.c
+++ reremake_01/get_parsed_data.c
@@ -6,10 +6,65 @@
 /*   By: hoysong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:26:00 by hoysong           #+#    #+#             */
-/*   Updated: 2024/07/07 14:53:40 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/07/09 04:31:47 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "my_fdf.h"
+
+//static void	to_upper(char *char_ptr)
+//{
+//	if (*char_ptr <= 'z' && *char_ptr >= 'a')
+//		*char_ptr -= 32;
+//}
+
+static int	color_atoi(char *src)
+{
+	int	int_value;
+
+	int_value = 0;
+	if (*src == ',')
+		src++;
+	if (*src == '0' && *(src + 1) == 'x')
+		src = src + 2;
+	while (*src)
+	{
+		if ('0' <= *src && *src <= '9')
+			int_value = (int_value * 16) + (*src - '0');
+		if ('A' <= *src && *src <= 'F')
+			int_value = (int_value * 16) + (*src - 55);
+		++src;
+	}
+	if (int_value == 0)
+		return (0xFFFFFF);
+	else
+		return (int_value);
+	return (0);
+}
+
+static int	z_atoi(char *z)
+{
+	int	z_value;
+
+	z_value = 0;
+	while (*z && *z != ',' && *z != '\n')
+	{
+		z_value = (z_value * 10) + (*z - '0');
+		z++;
+	}
+	return (z_value);
+}
+
+static t_point	**splits_to_points(t_prs_data *prs_data, char ***splits)
+{
+	t_point	**point_arr;
+
+	point_arr = (t_point **)malloc(sizeof(t_point *) * prs_data->file_lines);
+	if(point_arr == 0)
+		return (0);
+	return (point_arr);
+	point_arr[0][0].z = z_atoi(&(splits[0][0][0]));
+	point_arr[0][0].color = color_atoi(&(splits[0][0][0]));
+}
 
 static void	free_splits(char ***splits)
 {
@@ -71,6 +126,7 @@ static t_dnode *read_file_with_gnl(int fd, t_prs_data *prs_data)
 			break ;
 	}
 	prs_data->file_lines --;
+	prs_data->vert = prs_data->file_lines;
 	printf("file_lines(gnl): %d\n", prs_data->file_lines);
 	return (node_head);
 }
@@ -83,6 +139,6 @@ t_prs_data	*get_parsed_data(int fd, t_prs_data *prs_data)
 	prs_data->gnl_node = read_file_with_gnl(fd, prs_data);
 	prs_data->splits = split_gnl_data(prs_data, prs_data->gnl_node);
 	free_splits(prs_data->splits);
-	//splits_to_int();
+	//splits_to_points();
 	return (prs_data);
 }
