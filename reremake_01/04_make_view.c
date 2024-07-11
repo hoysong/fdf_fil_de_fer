1dfc7a248b46a7eff3d3e2725ec21a359e237aab
Hoyoung Song <hoysong@c3r9s6.42gyeongsan.kr>
2024-07-12 05:56:11 +0900

    점은 잘 찍힌다. 하지만 뒤집해서 찍히는 상황이다. 이전 커밋 내역을 돌려서 대부분의 파일을 찍어낼 수 있도록 하였다. norm도 맞춰야 한다. 일단 브레젠햄 알고리즘으로 점을 찍어보도록 하자.

--- reremake_01/04_make_view.c
+++ reremake_01/04_make_view.c
@@ -6,12 +6,11 @@
 /*   By: hoysong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 02:52:37 by hoysong           #+#    #+#             */
-/*   Updated: 2024/07/12 04:53:51 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/07/12 05:55:46 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "my_fdf.h"
 
-//put pixel in img buffer
 static void	my_mlx_pixel_put(t_img_strc *img_data, int x, int y, int color)
 {
 	char *dst;
@@ -20,14 +19,52 @@ static void	my_mlx_pixel_put(t_img_strc *img_data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+static void	get_low_num(int x, int y, t_prs_data *prs_data, t_img_strc *img_data)
+{
+	int	i;
+	int	j;
+	static	t_mv mv;
+	t_point	**point;
+
+	point = prs_data->point;
+	if (img_data == 0 )
+	{
+		if (mv.mv_x > x && x < 0)
+			mv.mv_x = x;
+		if (mv.mv_y > y && y < 0)
+			mv.mv_y = y;
+	}
+	else
+	{
+		mv.mv_x *= -1;
+		mv.mv_y *= -1;
+		printf("%d %d\n", mv.mv_x, mv.mv_y);
+		i = 0;
+		while(i < prs_data->vert)
+		{
+			j = 0;
+			while(j < prs_data->horiz)
+			{
+				prs_data->point[i][j].x += mv.mv_x;
+				prs_data->point[i][j].y += mv.mv_y;
+				my_mlx_pixel_put(img_data, point[i][j].x, point[i][j].y, point[i][j].color);
+				j++;
+			}
+			i++;
+		}
+		db_point_xy_prs_data(prs_data);
+	}
+}
+//put pixel in img buffer
+
 static void	ft_rotate_z(int *x, int *y, double z_angle)
 {
 	t_point	prev;
 
 	prev.x = *x;
 	prev.y = *y;
-	*x = (prev.x * cos(z_angle) - prev.y * sin(z_angle)) * 1;
-	*y = (prev.x * sin(z_angle) + prev.y * cos(z_angle)) * 1;
+	*x = (prev.x * cos(z_angle) - prev.y * sin(z_angle)) * 30;
+	*y = (prev.x * sin(z_angle) + prev.y * cos(z_angle)) * 30;
 }
 
 
@@ -100,12 +137,13 @@ void iso_prjc(t_img_strc *img_data, t_prs_data *prs_data)
 		while(j < prs_data->horiz)
 		{
 			ft_rotate_z(&(point[i][j].x), &(point[i][j].y), 45 * M_PI / 180); //바라보는 기준 45도 회전
-			point[i][j].z *= 5; // z 1당 높이값을 지정.
+			point[i][j].z *= 40; // z 1당 높이값을 지정.
 			point[i][j].y += point[i][j].z; // z에 곱한 값을 y에 적용한다.
 			point[i][j].y *= 0.5; // y의 값에 0.5와 같이 곱셈을 하여 찌부시킨다.
+			get_low_num(point[i][j].x, point[i][j].y, prs_data, 0);
 			j++;
 		}
 		i++;
 	}
-	get_low_num_2(prs_data, img_data);
+	get_low_num(0, 0, prs_data, img_data);
 }
