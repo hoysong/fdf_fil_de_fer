de4c1074a030745a4afed2ea8fe63bf461edd5f6
hoysong_firebat <hoysong@student.42gyeongsan.kr>
2024-07-24 07:45:41 +0900

    맵 밖으로 나가는 경우 맵 안에 들어갈 수 있도록 코드를 작성하였습니다. get_scale_data파일이 해당 파일입니다. x 축 기준으로 화면 밖에 나가는 경우에는 잘 동작하는 상태입니다. y 축 기준으로 나갈 경우의 상황도 추가해야 합니다. 누수가 없는 안정적인 커밋 상태입니다.
    1. y축 기준으로 화면 밖에 맵이 나가지 않도록 하는 로직도 추가하기.
    	(x축 나가지 않는 상황을 참고하여 제작할 것.)
    2. 파일명 수정하기

--- 05_get_scale_data.c
+++ 05_get_scale_data.c
@@ -6,7 +6,7 @@
 /*   By: hoysong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 07:56:42 by hoysong           #+#    #+#             */
-/*   Updated: 2024/07/24 05:52:00 by hoysong          ###   ########.fr       */
+/*   Updated: 2024/07/24 07:40:37 by hoysong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,32 +33,70 @@ static void	get_bigst_data(t_prs_data *prs_data, t_mv *cdnate)
 		}
 		i++;
 	}
-	printf("cdnate x; %d\n", cdnate->mv_x);
-	printf("cdnate y; %d\n", cdnate->mv_y);
+	printf("origin cdnate x; %d\n", cdnate->mv_x);
+	printf("origin cdnate y; %d\n", cdnate->mv_y);
 }
 
-static void	calc_gap(t_prs_data *prs_data, t_mv *cdnate)
+static double	calc_gap(t_prs_data *prs_data, t_mv *cdnate)
 {
-	if (cdnate->mv_x > 1000)
+//	int	now_gap;
+//	int	gap_count;
+//	
+//	gap_count = 0;
+//	now_gap = cdnate->mv_x / prs_data->horiz;
+//	while (now_gap * prs_data->horiz > 1000)
+//	{
+//		now_gap -= 1;
+//		gap_count ++;
+//		printf("%d %d %d %d\n", now_gap, prs_data->horiz * now_gap, gap_count, gap_count / (cdnate->mv_x / prs_data->horiz) * 100);
+//	}
+	double	new_gap;
+	if (cdnate->mv_x - 1000 > cdnate->mv_y - 800)
 	{
-		printf("new_gap x; %d\n", cdnate->mv_x);
-		printf("new_gap y; %d\n", cdnate->mv_y);
+		new_gap = cdnate->mv_x - 1000;
+		new_gap = 1 - (new_gap / cdnate->mv_x);
+		//new_gap = 100 * (cdnate->mv_x - 1000) / cdnate->mv_x;
+		printf("gapgapgap; %f\n", new_gap);
 	}
+	return (new_gap);
 }
 
-static void	apply_gap(t_prs_data *prs_data, t_mv *cdnate)
+static void	apply_gap(t_prs_data *prs_data, t_mv *cdnate, double new_gap)
 {
+	int	i;
+	int	j;
+	t_point **point;
+
+	i = 0;
+	j = 0;
+	point = prs_data->point;
+	while (i < prs_data->vert)
+	{
+		j = 0;
+		while (j < prs_data->horiz)
+		{
+			point[i][j].x *= (new_gap);
+			point[i][j].y *= (new_gap);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	resize_scale(t_prs_data *prs_data)
 {
 	t_mv cdnate;
+	double new_gap;
 
 	cdnate.mv_x = 0;
 	cdnate.mv_y = 0;
 	get_bigst_data(prs_data, &(cdnate));
+	if (cdnate.mv_x < 1000 && cdnate.mv_y < 800)
+	{
+		return ;
+	}
+	new_gap = calc_gap(prs_data, &(cdnate));
 	printf("new_gap x; %d\n", cdnate.mv_x);
 	printf("new_gap y; %d\n", cdnate.mv_y);
-	calc_gap(prs_data, &(cdnate));
-	apply_gap(prs_data, &(cdnate));
+	apply_gap(prs_data, &(cdnate), new_gap);
 }
